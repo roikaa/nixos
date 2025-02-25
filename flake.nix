651680e3b00fe828aebb47277ff1514a5ce07f2a
roikaa <akio216216@gmail.com>
2025-02-25 12:09:56 +0000

    .

--- flake.nix
+++ flake.nix
@@ -13,15 +13,18 @@
   outputs = { self, nixpkgs, ... }@inputs: 
     let
       system = "x86_64-linux";
+      host = "default";
       pkgs = nixpkgs.legacyPackages.${system};
     in
       {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/default/configuration.nix
-         inputs.home-manager.nixosModules.default
-      ];
+    nixosConfigurations = {
+      "{$host}" = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/default/configuration.nix
+           inputs.home-manager.nixosModules.default
+        ];
+      };
     };
   };
 }
