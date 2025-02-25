c68c990a697df58544ef5c0a61f213634e77038d
roikaa <akio216216@gmail.com>
2025-02-25 12:48:11 +0000

    fixng cursor

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
      home-manager = {
-       url = "github:nix-community/home-manager";
+       url = "github:nix-community/home-manager/release-24.11";
        inputs.nixpkgs.follows = "nixpkgs";
      };
   };
@@ -17,14 +17,12 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in
       {
-    nixosConfigurations = {
-      "{$host}" = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs;};
         modules = [
           ./hosts/default/configuration.nix
            inputs.home-manager.nixosModules.default
         ];
-      };
     };
   };
 }
