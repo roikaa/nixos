f4ff7e83d619d89315650d18b01eff26c0320ea9
roikaa <akio216216@gmail.com>
2025-09-30 00:17:59 +0100

    kde connect

--- hosts/kaze/home.nix
+++ hosts/kaze/home.nix
@@ -16,7 +16,6 @@
     ./../../user/app/pass/pass.nix
     ./../../user/style/stylix.nix
     ./../../user/cli/btop.nix
-    ./../../user/app/kde/kdeconnect.nix
 #     inputs.nixvim.homeModules.nixvim
   ];
 
