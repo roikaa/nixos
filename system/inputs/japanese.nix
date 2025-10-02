a885a7e6e10e1d49c7c96b0684e76996da84b22e
roikaa <akio216216@gmail.com>
2025-10-02 02:40:08 +0100

    JAPANESE

--- system/inputs/japanese.nix
+++ system/inputs/japanese.nix
@@ -1,77 +1,33 @@
 { config, lib, pkgs, ...}:
-
 {
   imports = [];
-
-  #############################################################
-  ## Select internationalisation properties for the console. ##
-  #############################################################
-
-  # The font to be used on the console when logging in.  Since most people
-  # only use Japanese in X, you can leave this as the default value.
-  i18n.consoleFont = "Lat2-Terminus16";
-
-  # The keymap to be used on the console.  Unless you use a special keyboard
-  # layout, you can leave this as the default value.
-  i18n.consoleKeyMap = "us";
-
-  # The locale to use by default.  Any locale is fine as long as the character
-  # set is "UTF-8".  If you want applications on your system to use Japanese by
-  # default, you could set the value to "ja_JP.UTF-8".
+  
+  # Console settings (no i18n prefix)
+  console.font = "Lat2-Terminus16";
+  console.keyMap = "us";
+  
+  # Locale setting (with i18n prefix)
   i18n.defaultLocale = "en_US.UTF-8";
+  
+  # Fonts
+  fonts.packages = with pkgs; [
+    carlito
+    ipafont
+    kochi-substitute
+  ];
 
-  ###################
-  ## Font Settings ##
-  ###################
-
-  # Enable fonts to use on your system.  You should make sure to add at least
-  # one English font (like dejavu_fonts), as well as Japanese fonts like
-  # "ipafont" and "kochi-substitute".
-#  fonts.fonts = with pkgs; [
-#    carlito
-#    dejavu_fonts
-#    ipafont
-#    kochi-substitute
-#    source-code-pro
-#    ttf_bitstream_vera
-#  ];
-#
-#  # Enable the "ultimate" font config.  This enables a few extra options to
-#  # make sure fonts look nice.  However, if you enable this and fonts look
-#  # strange, try disabling it.
-#  fonts.fontconfig.ultimate.enable = true;
-#
-#  # These settings enable default fonts for your system.  This setting is very
-#  # important.  It lets fontconfig know that you want to fall back to a Japanese
-#  # font (for example "IPAGothic") if an application tries to show fonts with
-#  # Japanese.  For instance, this is important if you are using a terminal
-#  # emulator and you `cat` some Japanese text to the screen. If you don't have
-#  # "defaultFonts" configured, fontconfig will pick a random Japanese font to
-#  # use.  If you have this "defaultFonts" setting configured, fontconfig will
-#  # pick the font you have selected.  This makes sure Japanese fonts look nice.
-#  fonts.fontconfig.defaultFonts = {
-#    monospace = [
-#      "DejaVu Sans Mono"
-#      "IPAGothic"
-#    ];
-#    sansSerif = [
-#      "DejaVu Sans"
-#      "IPAPGothic"
-#    ];
-#    serif = [
-#      "DejaVu Serif"
-#      "IPAPMincho"
-#    ];
-#  };
-#
-  ###############################
-  ## Input Method Editor (IME) ##
-  ###############################
-
-  # This enables "fcitx" as your IME.  This is an easy-to-use IME.  It supports many different input methods.
-  i18n.inputMethod.enabled = "fcitx";
-
-  # This enables "mozc" as an input method in "fcitx".  This has a relatively
-  # complete dictionary.  I recommend it for Japanese input.
-  i18n.inputMethod.fcitx.engines = with pkgs.fcitx-engines; [ mozc ];
+  # Environment variables for IME
+  environment.sessionVariables = {
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
+    GLFW_IM_MODULE = "ibus";
+  };
+  
+  # Input Method Editor
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [ fcitx5-mozc ];
+  };
 }
--- user/wm/hyprland/hyprland.nix
+++ user/wm/hyprland/hyprland.nix
@@ -1,79 +1,83 @@
 { config, pkgs, lib, ... }:  
 
 {
-    wayland.windowManager.hyprland = {
-      enable = true;
+  wayland.windowManager.hyprland = {
+    enable = true;
 
 
-      settings = {
-        
-        windowrule = "opacity 0.7, class:foot";
+    settings = {
 
-        exec-once = "swww init";
-          
-          "$mod" = "SUPER";
-          "$terminal" = "foot";
-          "$appluncher" = "rofi";
+      windowrule = "opacity 0.7, class:foot";
+
+      exec-once = ["swww init"
+      "fcitx5 -d --replace"];
+
+      "$mod" = "SUPER";
+      "$terminal" = "foot";
+      "$appluncher" = "rofi";
 
       input = {
-        repeat_delay = "300";
-        repeat_rate = "50";
+	repeat_delay = "300";
+	repeat_rate = "50";
+	kb_layout = "us";
+#	kb_variant = ",,,";
+#	kb_options = "grp:win_space_toggle";
       };
 
       decoration = {
-        rounding = 5 ;
-        active_opacity = 1;
-        inactive_opacity = 1;
-        
-        blur = {
-        enabled = true;
-          size = 5;
-          passes = 1;
-          vibrancy = 0.1696;
-        };
+	rounding = 5 ;
+	active_opacity = 1;
+	inactive_opacity = 1;
+
+	blur = {
+	  enabled = true;
+	  size = 5;
+	  passes = 1;
+	  vibrancy = 0.1696;
+	};
       };
 
       animations = {
-        enabled = false;
+	enabled = false;
       };
 
-    bindm = [
-      # mouse movements
-      "$mod, mouse:272, movewindow"
-      "$mod, mouse:273, resizewindow"
-      "$mod ALT, mouse:272, resizewindow"
-    ];
-    bind =
-      [
-        "$mod, Y, exec, foot yazi&,"
-        "$mod, W, exec, librewolf&,"
-        "$mod, return, exec, $terminal"
-        "$mod, Q, killactive,"
-        "$mod, D, exec, $appluncher -show drun"
-        "$mod, C, exec, rofi-pass"
-        "$mod, V, togglefloating,"
-        "$mod, P, exec, hyprshot -m output"
+      bindm = [
+# mouse movements
+	"$mod, mouse:272, movewindow"
+	  "$mod, mouse:273, resizewindow"
+	  "$mod ALT, mouse:272, resizewindow"
+      ];
+      bind =
+	[
+	"$mod, Y, exec, foot yazi&,"
+	  "$mod, W, exec, librewolf&,"
+	  "$mod, return, exec, $terminal"
+	  "$mod, Q, killactive,"
+	  "$mod, D, exec, $appluncher -show drun"
+	  "$mod, C, exec, rofi-pass"
+	  "$mod, V, togglefloating,"
+	  "$mod, P, exec, hyprshot -m output"
 
-        # special workspaces (scratchpad) 
-        "$mod, S, togglespecialworkspace, magic"
-        "$mod SHIFT, S, movetoworkspace, special:magic"
-      ]
-      ++ (
-        # workspaces
-        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-        builtins.concatLists (builtins.genList (
-            x: let
-              ws = let
-                c = (x + 1) / 10;
-              in
-                builtins.toString (x + 1 - (c * 10));
-            in [
-              "$mod, ${ws}, workspace, ${toString (x + 1)}"
-              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-            ]
-          )
-          10)
-      );
-      };
+# special workspaces (scratchpad) 
+	  "$mod, S, togglespecialworkspace, magic"
+	  "$mod SHIFT, S, movetoworkspace, special:magic"
+	]
+	++ (
+# workspaces
+# binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+	    builtins.concatLists (builtins.genList (
+		x: let
+		ws = let
+		c = (x + 1) / 10;
+		in
+		builtins.toString (x + 1 - (c * 10));
+		in [
+		"$mod, ${ws}, workspace, ${toString (x + 1)}"
+		"$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+		]
+		)
+	      10)
+	   );
     };
+  };
 }
