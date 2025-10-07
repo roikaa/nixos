d5f40cec681d36f2c917c370db99b3656c12de07
roikaa <akio216216@gmail.com>
2025-10-07 22:46:32 +0100

    rofi style

--- user/app/appluncher/rofi.nix
+++ user/app/appluncher/rofi.nix
@@ -1,26 +1,134 @@
 {pkgs, lib, config, ...}:
 {
-  programs.rofi = {
-    enable = true;
-#pass.enable = true;
+programs.rofi = {
+  enable = true;
+  
+  extraConfig = {
+    modi = "drun,run,filebrowser";
+    show-icons = true;
+    display-drun = "";
+    display-run = "";
+    display-filebrowser = "";
+  #  display-window = "";
+    drun-display-format = "{name} [<span weight='light' size='small'><i>({generic})</i></span>]";
+    window-format = "{w} · {c} · {t}";
+  };
 
-    extraConfig = {
-      show-icons = true;
-      width = 900;
-      lines = 15;
-      columns = 1;
-      dpi = 120;  # Increase this value for bigger icons (default is usually 96)
-        spacing = 2;
-    };
-    theme = let
+  theme = 
+    let
       inherit (config.lib.formats.rasi) mkLiteral;
     in {
-      "*" = {
-        icon-size = mkLiteral "52px";  # Change this value for different icon sizes (24px, 48px, etc.)
+      "window" = {
+       # location = lib.mkForce (mkLiteral "east");
+        #anchor = lib.mkForce (mkLiteral "east");
+        fullscreen = lib.mkForce false;
+        height = lib.mkForce (mkLiteral "50%");
+        width = lib.mkForce (mkLiteral "500px");
+        x-offset = lib.mkForce (mkLiteral "0px");
+        y-offset = lib.mkForce (mkLiteral "0px");
+        margin = lib.mkForce (mkLiteral "0px");
+        padding = lib.mkForce (mkLiteral "0px");
+        border = lib.mkForce (mkLiteral "0px solid");
+        border-radius = lib.mkForce (mkLiteral "0px");
       };
-  };
 
-  };
-}
+      "mainbox" = {
+        spacing = lib.mkForce (mkLiteral "15px");
+        margin = lib.mkForce (mkLiteral "0px");
+        padding = lib.mkForce (mkLiteral "45px");
+        border = lib.mkForce (mkLiteral "0px solid");
+        border-radius = lib.mkForce (mkLiteral "0px 0px 0px 0px");
+        children = lib.mkForce (map mkLiteral [ "inputbar" "message" "listview" "mode-switcher" ]);
+      };
+
+      "inputbar" = {
+        spacing = lib.mkForce (mkLiteral "10px");
+        margin = lib.mkForce (mkLiteral "0px");
+        padding = lib.mkForce (mkLiteral "0px");
+        border = lib.mkForce (mkLiteral "0px solid");
+        border-radius = lib.mkForce (mkLiteral "8px");
+        children = lib.mkForce (map mkLiteral [ "prompt" "entry" ]);
+      };
+
+      "entry" = {
+        placeholder = lib.mkForce "search...";
+      };
+
+      "listview" = {
+        columns = lib.mkForce 1;
+        lines = lib.mkForce 20;
+        cycle = lib.mkForce true;
+        dynamic = lib.mkForce true;
+        scrollbar = lib.mkForce false;
+        fixed-height = lib.mkForce true;
+        fixed-columns = lib.mkForce true;
+        spacing = lib.mkForce (mkLiteral "6px");
+        margin = lib.mkForce (mkLiteral "0px");
+        padding = lib.mkForce (mkLiteral "0px");
+        border = lib.mkForce (mkLiteral "0px solid");
+        border-radius = lib.mkForce (mkLiteral "0px");
+      };
+
+      "scrollbar" = {
+        handle-width = lib.mkForce (mkLiteral "5px");
+        border-radius = lib.mkForce (mkLiteral "8px");
+      };
+
+      "element" = {
+        spacing = lib.mkForce (mkLiteral "10px");
+        margin = lib.mkForce (mkLiteral "0px");
+        padding = lib.mkForce (mkLiteral "5px");
+        border = lib.mkForce (mkLiteral "0px solid");
+        border-radius = lib.mkForce (mkLiteral "5px");
+      };
+
+      "element-icon" = {
+        border-radius = lib.mkForce (mkLiteral "5px");
+        padding = lib.mkForce (mkLiteral "5px");
+        size = lib.mkForce (mkLiteral "20px");
+      };
+
+      "element-text" = {
+        vertical-align = lib.mkForce (mkLiteral "0.5");
+        horizontal-align = lib.mkForce (mkLiteral "0.0");
+      };
+
+      "mode-switcher" = {
+        spacing = lib.mkForce (mkLiteral "7px");
+        margin = lib.mkForce (mkLiteral "0px");
+        padding = lib.mkForce (mkLiteral "0px");
+        border = lib.mkForce (mkLiteral "0px solid");
+        border-radius = lib.mkForce (mkLiteral "0px");
+      };
+
+      "button" = {
+        padding = lib.mkForce (mkLiteral "15px");
+        border = lib.mkForce (mkLiteral "0px solid");
+        border-radius = lib.mkForce (mkLiteral "4px");
+      };
+
+      "message" = {
+        margin = lib.mkForce (mkLiteral "0px");
+        padding = lib.mkForce (mkLiteral "0px");
+        border = lib.mkForce (mkLiteral "0px solid");
+        border-radius = lib.mkForce (mkLiteral "0px 0px 0px 0px");
+      };
+
+      "textbox" = {
+        padding = lib.mkForce (mkLiteral "8px");
+        border = lib.mkForce (mkLiteral "0px solid");
+        border-radius = lib.mkForce (mkLiteral "4px");
+        vertical-align = lib.mkForce (mkLiteral "0.5");
+        horizontal-align = lib.mkForce (mkLiteral "0.0");
+      };
+
+      "error-message" = {
+        padding = lib.mkForce (mkLiteral "10px");
+        border = lib.mkForce (mkLiteral "0px solid");
+        border-radius = lib.mkForce (mkLiteral "4px");
+      };
+    };
+};
+ }
 
 
