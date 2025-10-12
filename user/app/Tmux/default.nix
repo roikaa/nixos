982b54d290652160180d9136c909ff875f47e557
roikaa <akio216216@gmail.com>
2025-10-12 09:45:53 +0100

    Tmux

--- user/app/Tmux/default.nix
+++ user/app/Tmux/default.nix
@@ -1,12 +1,22 @@
 {pkgs, lib, config, ...}:
 {
-    programs.tmux = {
-      enable = true;
-      clock24 = true;
-    };
-    
-    home.file.".config/tmux" = {
-    	source = ./tmux.conf;
-        recursive = true;
-    };
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+    keyMode = "vi";
+    plugins = with pkgs; [
+      tmuxPlugins.cpu
+      {
+        plugin = tmuxPlugins.resurrect;
+        extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+      }
+      {
+        plugin = tmuxPlugins.continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '60' # minutes
+        '';
+      }
+    ] 
+  };
 }
