64032c17279fa85518887e1e1e13810ee4e7af78
roikaa <akio216216@gmail.com>
2025-10-12 11:38:04 +0100

    TmuxConfiguration

--- user/app/Tmux/default.nix
+++ user/app/Tmux/default.nix
@@ -3,8 +3,17 @@
   programs.tmux = {
     enable = true;
     clock24 = true;
+    mouse = true;
     keyMode = "vi";
+    prefix = "C-Space";
+    extraConfig = ''
+      bind -n M-H previous-window
+      bind -n M-L next-window 
+
+         '';
     plugins = with pkgs; [
+      tmuxPlugins.better-mouse-mode
+      tmuxPlugins.vim-tmux-navigator
       tmuxPlugins.cpu
       {
         plugin = tmuxPlugins.resurrect;
@@ -17,6 +26,6 @@
           set -g @continuum-save-interval '60' # minutes
         '';
       }
-    ]; 
+    ];
   };
 }
