8ec576d1216c86677daaaa5a22db861df67ad67f
roikaa <akio216216@gmail.com>
2025-10-12 12:39:19 +0100

    TmuxConfiguration

--- user/app/Tmux/default.nix
+++ user/app/Tmux/default.nix
@@ -7,12 +7,15 @@
     keyMode = "vi";
     prefix = "C-Space";
     extraConfig = ''
-      bind -n M-H previous-window
-      bind -n M-L next-window
+      bind -n M-h previous-window
+      bind -n M-l next-window
       
-      bind "" split-window -v -c "#{pane_current_path}"
-      bind % split-window -h -c "#{pane_current_path}"
-      
+      # bind '"' split-window -v -c "#{pane_current_path}"
+      # bind % split-window -h -c "#{pane_current_path}"
+      bind-key -T prefix v split-window -v -c "#{pane_current_path}"
+      bind-key -T prefix h split-window -h -c "#{pane_current_path}"
+
+
       set -g pane-base-index 1
       set -g base-index 1
       set-window-option -g pane-base-index 1
