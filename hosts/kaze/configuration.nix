abe5f6c308bed99b7a642ebd7daa07c10f524c12
roikaa <akio216216@gmail.com>
2025-12-25 21:55:10 +0100

    sddm theam

--- hosts/kaze/configuration.nix
+++ hosts/kaze/configuration.nix
@@ -1,8 +1,7 @@
 { config, lib, pkgs, inputs, ... }:
-let 
-     tokyo-night-sddm = pkgs.libsForQt5.callPackage ./../../system/sddm/tokyo-night.nix { };
-in 
-
+# let 
+     # tokyo-night-sddm = pkgs.libsForQt5.callPackage ./../../system/sddm/tokyo-night.nix { };
+# in 
 {
   imports =
     [ # Include the results of the hardware scan.
