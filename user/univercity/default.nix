7ef431d9c151d28acfd1dffed7481f7489a55af4
roikaa <akio216216@gmail.com>
2025-12-27 22:05:22 +0100

    update

--- user/univercity/default.nix
+++ user/univercity/default.nix
@@ -3,8 +3,8 @@
   environment.systemPackages = with pkgs; [
     # ciscoPacketTracer8
     
-    # gns3-gui
-    # gns3-server
+    gns3-gui
+    gns3-server
   
     # qemu
     # dynamips
@@ -18,8 +18,8 @@
     
     # Python and networking tools
     python3
-    python3Packages.pip
-    python3Packages.netmiko
+    # python3Packages.pip
+    # python3Packages.netmiko
     
     # Useful networking tools
     busybox
@@ -28,18 +28,18 @@
     tcpdump
   ];
 
-  virtualisation.libvirtd.enable = true;
-
-  users.groups.ubridge = { }; # Explicitly defines the 'ubridge' group
-  users.users.akio.extraGroups = ["kvm" "libvirtd" "ubridge" "gns3" "wireshark"];
-
- security.wrappers.ubridge = {
-  source = "/run/current-system/sw/bin/ubridge";
-  capabilities = "cap_net_admin,cap_net_raw+ep";
-  owner = "root";
-  group = "ubridge"; # You will need to create this group
-  # permissions = "u+rx,g+x";
-    permissions = "u+rx,g+rx,o+rx";
-}; 
+  # virtualisation.libvirtd.enable = true;
+#
+#   users.groups.ubridge = { }; # Explicitly defines the 'ubridge' group
+#   users.users.akio.extraGroups = ["kvm" "libvirtd" "ubridge" "wireshark"]; # "gns3" 
+#
+#  security.wrappers.ubridge = {
+#   source = "/run/current-system/sw/bin/ubridge";
+#   capabilities = "cap_net_admin,cap_net_raw+ep";
+#   owner = "root";
+#   group = "ubridge"; # You will need to create this group
+#   # permissions = "u+rx,g+x";
+#     permissions = "u+rx,g+rx,o+rx";
+# }; 
     
 }
