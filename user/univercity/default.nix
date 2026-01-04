b94690ef2b5f0b3a0136f4fb296c26b0f3e78df8
roikaa <akio216216@gmail.com>
2026-01-04 10:44:48 +0100

    update

--- user/univercity/default.nix
+++ user/univercity/default.nix
@@ -1,18 +1,13 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    # ciscoPacketTracer8
-    
     gns3-gui
     gns3-server
     dynamips
     vpcs
-    ubridge
+    # ubridge
 
     xterm              # Classic, lightweight
-    # konsole          # KDE (if using KDE)
-    # gnome.gnome-terminal  # GNOME (if using GNOME)
-    # alacritty        # Modern, GPU-accelerated
     
     # Python and networking tools
     python3
@@ -28,17 +23,7 @@
 
   virtualisation.libvirtd.enable = true;
 #
-#   users.groups.ubridge = { }; # Explicitly defines the 'ubridge' group
   users.users.akio.extraGroups = ["kvm" "libvirtd" "wireshark"]; # "gns3" 
-#
-#  security.wrappers.ubridge = {
-#   source = "/run/current-system/sw/bin/ubridge";
-#   capabilities = "cap_net_admin,cap_net_raw+ep";
-#   owner = "root";
-#   group = "ubridge"; # You will need to create this group
-#   # permissions = "u+rx,g+x";
-#     permissions = "u+rx,g+rx,o+rx";
-# }; 
 
   security.wrappers.ubridge = {
     source = "${pkgs.ubridge}/bin/ubridge";
