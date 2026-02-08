5a6e2523b927f7837a3f7c6b5d74ef1bafc6ab89
roikaa <akio216216@gmail.com>
2026-02-08 15:55:54 +0100

    dark/light

--- system/style/stylix.nix
+++ system/style/stylix.nix
@@ -1,53 +1,43 @@
-{ pkgs, ... }:
+{ config, pkgs, lib, inputs, ... }: 
 {
   stylix = {
-
     enable = true;
-   
-    image = ./white-mountain.jpg;
-    # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-light-medium.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
     targets = {
-    	gtk.enable = true;
-    	qt.enable = true;
-        # zathura.enable = true;
+      gtk.enable = true;
+      qt.enable = true;
     };
-    # polarity = "dark";
-
-
-    opacity = {
-      #applications = 1.0;
-      #terminal = 0.7;
-      #desktop = 1.0;
-      #popups = 1.0;
-    };
-
-
+    polarity = "dark";
     fonts = {
       sizes = {
         terminal = 15;
-	applications = 12;
+        applications = 12;
         desktop = 11;
         popups = 12;
       };
-    serif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Serif";
-    };
-
-    sansSerif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Sans";
-    };
-
-    monospace = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Sans Mono";
-    };
-
-    emoji = {
-      package = pkgs.noto-fonts-emoji;
-      name = "Noto Color Emoji";
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+      monospace = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans Mono";
+      };
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
     };
   };
-};
+  
+  specialisation.light.configuration = {
+    stylix = {
+      base16Scheme = lib.mkForce "${pkgs.base16-schemes}/share/themes/gruvbox-light-medium.yaml";
+      polarity = lib.mkForce "light";
+    };
+  };
 }
--- user/style/stylix.nix
+++ user/style/stylix.nix
@@ -1,15 +1,26 @@
-{ config, pkgs, inputs, ... }: 
+{ config, pkgs, lib, inputs, ... }: 
 {
-stylix.targets.firefox.profileNames = [ "roika" ];
-# this is home-manager stylix
+  home.packages = [
+    (pkgs.writeShellApplication {
+      name = "toggle-gruvbox";
+      runtimeInputs = [ pkgs.ripgrep ];
+      text = ''
+        gen=$(home-manager generations | head -1 | rg -o '/nix/store/[^ ]*')
+        if [[ "$gen" =~ light ]]; then
+          "$(home-manager generations | sed -n 2p | rg -o '/nix/store/[^ ]*')/activate"
+        else
+          "$gen/specialisation/light/activate"
+        fi
+      '';
+    })
+  ];
+
   stylix = {
     enable = true;
-    # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-light-medium.yaml";
-    image = ../../system/style/white-mountain.jpg;
-
-    # polarity = "dark"; # Hopfully this force dark theme
-
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+    polarity = "dark";
     targets = {
+      firefox.profileNames = [ "roika" ];
       neovim.enable = true;
       rofi.enable = true;
       foot.enable = true;
@@ -20,26 +31,25 @@ stylix.targets.firefox.profileNames = [ "roika" ];
       waybar.enable = true;
       zathura.enable = true;
     };
-
-autoEnable = false;  
+    autoEnable = false;  
     cursor = {
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Ice";
       size = 24;
     };
-
-    # iconTheme = { 
-      # enable = true;
-      # package = pkgs.papirus-icon-theme;
-      # dark = "Papirus";
-    # };
-
-        # fonts.sizes.applications = 14;
-   iconTheme = {
-     enable = true;
-     package = pkgs.gruvbox-plus-icons;
-     light = "Gruvbox-Plus-Light";
-     dark = "Gruvbox-Plus-Dark";
-   };
+    iconTheme = {
+      enable = true;
+      package = pkgs.gruvbox-plus-icons;
+      light = "Gruvbox-Plus-Light";
+      dark = "Gruvbox-Plus-Dark";
+    };
+  };
+  
+  # Add home-manager specialisation
+  specialisation.light.configuration = {
+    stylix = {
+      base16Scheme = lib.mkForce "${pkgs.base16-schemes}/share/themes/gruvbox-light-medium.yaml";
+      polarity = lib.mkForce "light";
+    };
   };
 }   
