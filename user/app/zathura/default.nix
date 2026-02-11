df7ada2ad7657a4d50a00376e2344ac2a75248e5
roikaa <akio216216@gmail.com>
2026-02-11 23:36:16 +0100

    zathura copy highlited into clip board

--- user/app/zathura/default.nix
+++ user/app/zathura/default.nix
@@ -2,55 +2,8 @@
 {
    programs.zathura = {
   enable = true;
-  # extraConfig = ''
-  #   # Notifications
-  #   set notification-error-bg       "rgba(40,40,40,1)"
-  #   set notification-error-fg       "rgba(251,73,52,1)"
-  #   set notification-warning-bg     "rgba(40,40,40,1)"
-  #   set notification-warning-fg     "rgba(250,189,47,1)"
-  #   set notification-bg             "rgba(40,40,40,1)"
-  #   set notification-fg             "rgba(184,187,38,1)"
-  #   
-  #   # Completion menu
-  #   set completion-bg               "rgba(80,73,69,1)"
-  #   set completion-fg               "rgba(235,219,178,1)"
-  #   set completion-group-bg         "rgba(60,56,54,1)"
-  #   set completion-group-fg         "rgba(146,131,116,1)"
-  #   set completion-highlight-bg     "rgba(131,165,152,1)"
-  #   set completion-highlight-fg     "rgba(80,73,69,1)"
-  #   
-  #   # Index mode
-  #   set index-bg                    "rgba(80,73,69,1)"
-  #   set index-fg                    "rgba(235,219,178,1)"
-  #   set index-active-bg             "rgba(131,165,152,1)"
-  #   set index-active-fg             "rgba(80,73,69,1)"
-  #   
-  #   # Input bar
-  #   set inputbar-bg                 "rgba(40,40,40,1)"
-  #   set inputbar-fg                 "rgba(235,219,178,1)"
-  #   
-  #   # Status bar
-  #   set statusbar-bg                "rgba(80,73,69,1)"
-  #   set statusbar-fg                "rgba(235,219,178,1)"
-  #   
-  #   # Highlighting
-  #   set highlight-color             "rgba(250,189,47,0.5)"
-  #   set highlight-active-color      "rgba(254,128,25,0.5)"
-  #   
-  #   # Default colors
-  #   set default-bg                  "rgba(40,40,40,1)"
-  #   set default-fg                  "rgba(235,219,178,1)"
-  #   
-  #   # Rendering
-  #   set render-loading              true
-  #   set render-loading-bg           "rgba(40,40,40,1)"
-  #   set render-loading-fg           "rgba(235,219,178,1)"
-  #   
-  #   # Recolor mode
-  #   set recolor-lightcolor          "rgba(40,40,40,1)"
-  #   set recolor-darkcolor           "rgba(235,219,178,1)"
-  #   set recolor                     true
-  #   set recolor-keephue             true
-  # '';
+  extraConfig = ''
+      set selection-clipboard clipboard
+  '';
 };
 }
