aa88f74ebd0c87bc929953976f6df5647cc74cfc
roikaa <akio216216@gmail.com>
2026-02-24 19:58:11 +0100

    styling

--- user/style/stylix.nix
+++ user/style/stylix.nix
@@ -57,7 +57,7 @@ in
   
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-hard.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
     polarity = "dark";
     targets = {
       librewolf.enable = true; 
