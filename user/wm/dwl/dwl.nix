7c734753abeedde08c840a8ae2b1e4ba3995357c
roikaa <akio216216@gmail.com>
2026-02-25 20:40:00 +0100

    dwl

--- user/wm/dwl/dwl.nix
+++ user/wm/dwl/dwl.nix
@@ -1,28 +1,23 @@
-{
-  # config,
-  pkgs,
-  # lib,
-  ...
-}: {
+{pkgs, ...}: {
   # Adding some dependencies
-  # home.packages = with pkgs; [
-  # wayland
-  # wayland-protocols
-  # wlroots_0_19
-  # foot
-  # git
-  # wmenu
-  # wl-clipboard
-  # grim
-  # slurp
-  # swaybg
-  # firefox
-  # jetbrains-mono
-  # ];
-
- home.packages = [
-    (pkgs.dwl.override {
-      configH = ./dwl-config.h;
-    })
-  ];
+  home.packages = with pkgs;
+    [
+      wayland
+      wayland-protocols
+      wlroots_0_19
+      # foot
+      # git
+      wmenu
+      wl-clipboard
+      grim
+      slurp
+      swaybg
+      # firefox
+      # jetbrains-mono
+    ]
+    ++ [
+      (pkgs.dwl.override {
+        configH = ./dwl-config.h;
+      })
+    ];
 }
