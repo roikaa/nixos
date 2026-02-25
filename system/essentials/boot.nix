a0e55da132586a9641e2daa5ae4a5694647fd584
roikaa <akio216216@gmail.com>
2026-02-25 19:37:13 +0100

    time to leave hypland

--- system/essentials/boot.nix
+++ system/essentials/boot.nix
@@ -1,12 +1,12 @@
 {pkgs, inputs, ...}: 
-let
-  cybergrub = pkgs.fetchFromGitHub {
-    owner = "adnksharp";
-    repo = "CyberGRUB-2077";
-    rev = "86ed7c3af18c3b69dd002b341dbb099daaf39eab"; # Or specific commit hash for reproducibility, e.g., "v1.0"
-    sha256 = "sha256-quXluKYzylSnUnbLZbzdygM5pgDwB1PgLV4VAU66Lc0="; # Compute via `nix-prefetch-github adnksharp CyberGRUB-2077`
-  };
-in
+# let
+  # cybergrub = pkgs.fetchFromGitHub {
+    # owner = "adnksharp";
+    # repo = "CyberGRUB-2077";
+    # rev = "86ed7c3af18c3b69dd002b341dbb099daaf39eab"; # Or specific commit hash for reproducibility, e.g., "v1.0"
+    # sha256 = "sha256-quXluKYzylSnUnbLZbzdygM5pgDwB1PgLV4VAU66Lc0="; # Compute via `nix-prefetch-github adnksharp CyberGRUB-2077`
+  # };
+# in
   {
   # boot.loader.systemd-boot.enable = true;
   boot.loader.efi.efiSysMountPoint = "/boot"; # CRITICAL - tells NixOS where EFI is
@@ -17,7 +17,7 @@ in
     device = "nodev";
     useOSProber = false;
     gfxmodeEfi = "auto";
-    theme = "${cybergrub}/CyberGRUB-2077";
+    theme = "./teleport-night-1280x720";
     # theme = inputs.grubshin-bootpact.night.teleport."1280x720";
 
   };
