87c8c76cd16c8768803f1e1b6657e6bf140cf3c9
roikaa <akio216216@gmail.com>
2026-02-25 18:42:57 +0100

    time to leave hypland

--- user/wm/dwl/dwl.nix
+++ user/wm/dwl/dwl.nix
@@ -5,39 +5,24 @@
   ...
 }: {
   # Adding some dependencies
-  home.packages = with pkgs; [
-    wayland
-    wayland-protocols
-    wlroots_0_19
-    foot
-    git
-    wmenu
-    wl-clipboard
-    grim
-    slurp
-    swaybg
-    firefox
-    jetbrains-mono
-  ];
+  # home.packages = with pkgs; [
+  # wayland
+  # wayland-protocols
+  # wlroots_0_19
+  # foot
+  # git
+  # wmenu
+  # wl-clipboard
+  # grim
+  # slurp
+  # swaybg
+  # firefox
+  # jetbrains-mono
+  # ];
 
-  programs.dwl = {
-    enable = true;
-    package =
-      pkgs.dwl.override
-      {
-        configH = ./dwl-config.h;
-      }.overrideAttrs (oldAttrs: {
-        buildInputs =
-          oldAttrs.buildInputs or []
-          ++ [
-            pkgs.libdrm
-            pkgs.fcft
-          ];
-        # patches =
-          # oldAttrs.patches or []
-          # ++ [
-            # ./bar-0.7.patch
-          # ];
-      });
-  };
+ home.packages = [
+    (pkgs.dwl.override {
+      configH = ./dwl-config.h;
+    })
+  ];
 }
