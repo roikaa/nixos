add080ca4b54eb74fb78e44c153dd1c0b78378a5
roikaa <akio216216@gmail.com>
2026-02-27 13:51:16 +0100

    GREEETER

--- hosts/nobu/configuration.nix
+++ hosts/nobu/configuration.nix
@@ -76,8 +76,8 @@ services.greetd = {
   enable = true;
   settings = {
     default_session = {
-      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd '${pkgs.dwl}/bin/dwl -s ${pkgs.waybar}/bin/waybar'--time";
-      user = "akio";
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet -t -c 'dwl -s waybar' ";
+      user = "greeter";
     };
   };
 };  # services.displayManager.sddm = {
